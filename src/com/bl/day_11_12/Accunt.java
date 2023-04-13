ad9047700992bb8f1b120de8b34d7da9e9c347b6
lunainfotec <kirtimadan1997@gmail.com>
2023-04-13 21:36:02 +0530

    added : accunt blanace

---
+++ src/com/bl/day_11_12/Accunt.java
@@ -0,0 +1,60 @@
+package com.bl.day_11_12;
+
+public class Accunt {
+    private double balance;
+
+
+    public Accunt(double inibalance){
+
+        if(inibalance > 0.0)
+
+            inibalance = 25000;
+
+        System.out.println("total amount :" + inibalance);
+
+    }
+
+    public void credit( double amount){
+
+        balance = balance + amount;
+        System.out.println(balance);
+    }
+
+    public  void debit (double debitamount){
+
+        if(debitamount > balance){
+
+            debitamount = 0.0;
+
+            System.out.println("check your balance");
+        }
+
+        balance = balance - debitamount;
+
+        System.out.println(balance);
+
+    }
+
+
+    public  double getBalance(){
+
+        return  balance;
+
+    }
+
+    public static void main(String[] args) {
+
+
+        Accunt obj = new Accunt(25000);
+         obj.credit(200);
+         obj.debit(50);
+
+
+
+
+
+
+
+    }
+
+}
